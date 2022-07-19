cbedf86ecc23ba4da367a1052d106dc4bb606f00
alena14good <a.pigeeva@gmail.com>
2022-07-19 15:39:07 +0300

    Added files

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,68 @@
+
+    import static org.junit.jupiter.api.Assertions.*;
+
+    public class BonusServiceTest {
+
+        @org.junit.jupiter.api.Test
+        void shouldCalculateForRegisteredAndUnderLimit() {
+            BonusService service = new BonusService();
+
+            // подготавливаем данные:
+            int amount = 1000;
+            boolean registered = true;
+            int expected = 30;
+
+            // вызываем целевой метод:
+            int actual = service.calculate(amount, registered);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            assertEquals(expected, actual);
+        }
+
+        @org.junit.jupiter.api.Test
+        void shouldCalculateForRegisteredAndOverLimit() {
+            BonusService service = new BonusService();
+
+            // подготавливаем данные:
+            int amount = 1_000_000;
+            boolean registered = true;
+            int expected = 500;
+
+            // вызываем целевой метод:
+            int actual = service.calculate(amount, registered);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            assertEquals(expected, actual);
+        }
+        @org.junit.jupiter.api.Test
+        void shouldCalculateForNotRegisteredAndOverLimit() {
+            BonusService service = new BonusService();
+
+            // подготавливаем данные:
+            int amount = 1_000_000;
+            boolean registered = false;
+            int expected = 500;
+
+            // вызываем целевой метод:
+            int actual = service.calculate(amount, registered);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            assertEquals(expected, actual);
+        }
+        @org.junit.jupiter.api.Test
+        void shouldCalculateForNotRegisteredAndUnderLimit() {
+            BonusService service = new BonusService();
+
+            // подготавливаем данные:
+            int amount = 1000;
+            boolean registered = false;
+            int expected = 10;
+
+            // вызываем целевой метод:
+            int actual = service.calculate(amount, registered);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            assertEquals(expected, actual);
+        }
+    }
+
